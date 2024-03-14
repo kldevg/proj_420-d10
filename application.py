326dac206afd03bda078eb70d28b12b35f4ae8e4
kld <kld@me>
2024-03-13 22:34:13 -0400

    update: ajout route vers page de contact

--- application.py
+++ application.py
@@ -8,5 +8,10 @@ def main():
     return "Message depuis Flask!"
 
 
+@application.route("/contact")
+def test():
+    return "Message de la page de contact!"
+
+
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=5555)
